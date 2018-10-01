e37c8b52757bd946f20fd76de13ee1aca2d4cb7d
Thomas DoyleEngler <tpdoyle87@gmail.com>
2018-10-01 16:26:24 -0300

    changes

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
 console.log('starting app');
+
 const fs = require('fs');
-const os = require('os');
+const _ = require('lodash');
 
-var user = os.userInfo();
-console.log(user)
-
-fs.appendFile('greetings.txt', `hello ${user.username}`, (err) => {
-  if (err) {
-    console.log("unable to append to the file")
-  }
-});
+const notes = require('./notes.js')
---
+++ notes.js
@@ -0,0 +1,6 @@
+console.log("starting notes.js")
+
+module.exports.addNote = () => {
+   console.log('add note')
+   return 'new note'
+};
